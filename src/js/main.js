3f7106f122e44a89b9abb8026d6bbce9bb01e6e0
上田貴央 <gibson511511@gmail.com>
2021-06-26 16:12:15 +0900

    focus-loop実装（バグがあるため一旦保留）

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import '../scss/app.scss';
+import { focusLoop } from './modules/_focusLoop.mjs';
 import { importFontAwesome } from './modules/_fontAwesome.mjs';
-import { GlobalMenu } from './modules/_hamburgerMenu.mjs';
+import { GlobalMenu } from './modules/_globalMenu.mjs';
 import { headerStyleChanger } from './modules/_l-header-styleChange.mjs';
 
 headerStyleChanger();
 GlobalMenu();
 importFontAwesome();
+focusLoop();
