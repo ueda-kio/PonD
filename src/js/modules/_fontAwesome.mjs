adf5c65c270479d802e0fbadd654b8b6396a47c8
上田貴央 <gibson511511@gmail.com>
2021-06-26 21:01:07 +0900

    news他ページ追加

--- src/js/modules/_fontAwesome.mjs
+++ src/js/modules/_fontAwesome.mjs
@@ -1,9 +1,20 @@
 import {dom, library} from '@fortawesome/fontawesome-svg-core';
-import {faAngleRight, faExternalLinkAlt, faAngleDoubleRight} from '@fortawesome/free-solid-svg-icons';
+import {faAngleRight, faExternalLinkAlt, faAngleDoubleRight, faThumbsUp, faChevronLeft, faChevronRight} from '@fortawesome/free-solid-svg-icons';
+import {faFacebook, faTwitter, faLine} from '@fortawesome/free-brands-svg-icons';
 
 export const importFontAwesome = () => {
 
-  library.add(faAngleRight, faExternalLinkAlt, faAngleDoubleRight);
+  library.
+    add(
+      faAngleRight,
+      faExternalLinkAlt,
+      faAngleDoubleRight, faThumbsUp,
+      faFacebook,
+      faTwitter,
+      faLine,
+      faChevronLeft,
+      faChevronRight
+    );
 
   dom.i2svg();
 };
