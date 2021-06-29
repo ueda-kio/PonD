c829b97d5e2b484598cb96885e21771aa9f983ab
上田貴央 <gibson511511@gmail.com>
2021-06-29 19:32:30 +0900

    - スライダー再生・停止ボタン修正 - ビルド

--- src/js/modules/_fontAwesome.mjs
+++ src/js/modules/_fontAwesome.mjs
@@ -1,22 +1,22 @@
-import {dom, library} from '@fortawesome/fontawesome-svg-core';
-import {faAngleRight, faExternalLinkAlt, faAngleDoubleRight, faThumbsUp, faChevronLeft, faChevronRight, faPauseCircle, faPlayCircle} from '@fortawesome/free-solid-svg-icons';
-import {faFacebook, faTwitter, faLine} from '@fortawesome/free-brands-svg-icons';
+// import {dom, library} from '@fortawesome/fontawesome-svg-core';
+// import {faAngleRight, faExternalLinkAlt, faAngleDoubleRight, faThumbsUp, faChevronLeft, faChevronRight, faPauseCircle, faPlayCircle} from '@fortawesome/free-solid-svg-icons';
+// import {faFacebook, faTwitter, faLine} from '@fortawesome/free-brands-svg-icons';
 
-export const importFontAwesome = () => {
+// export const importFontAwesome = () => {
 
-  library.
-    add(
-      faAngleRight,
-      faExternalLinkAlt,
-      faAngleDoubleRight, faThumbsUp,
-      faFacebook,
-      faTwitter,
-      faLine,
-      faChevronLeft,
-      faChevronRight,
-      faPauseCircle,
-      faPlayCircle
-    );
+//   library.
+//     add(
+//       faAngleRight,
+//       faExternalLinkAlt,
+//       faAngleDoubleRight, faThumbsUp,
+//       faFacebook,
+//       faTwitter,
+//       faLine,
+//       faChevronLeft,
+//       faChevronRight,
+//       faPauseCircle,
+//       faPlayCircle
+//     );
 
-  dom.i2svg();
-};
+//   dom.i2svg();
+// };
