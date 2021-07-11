781527b9c80c6408628596f4e3de2ee77f27f172
上田貴央 <gibson511511@gmail.com>
2021-07-11 11:52:13 +0900

    validation実装

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import '../scss/app.scss';
 import { focusLoop } from './modules/_focusLoop.mjs';
+import './modules/Class/_FormValidation.mjs'
 // import { importFontAwesome } from './modules/_fontAwesome.mjs';
 import { GlobalMenu } from './modules/_globalMenu.mjs';
 import { headerStyleChanger } from './modules/_l-header-styleChange.mjs';
