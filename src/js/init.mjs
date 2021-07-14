ba3d7edbe24f60bffd43efeb711fd17d08ac3a53
上田貴央 <gibson511511@gmail.com>
2021-07-14 18:58:52 +0900

    init.js実装

---
+++ src/js/init.mjs
@@ -0,0 +1,23 @@
+/**
+ * ! IEで動作しないシンタックスを利用しないこと
+ * @see common\_inc\_head-script.ejs
+ */
+const html = document.documentElement;
+export const init = () => {
+  if (
+    html &&
+    html.nodeType === Node.ELEMENT_NODE
+  ) {
+    // JS有効時のセレクタとして利用するカスタムdata属性を付与
+    html.dataset.scriptEnabled = 'true';
+
+    // Trident (IE) 判定
+    if ('uniqueID' in document) {
+        html.dataset.browserTrident = 'true';
+    }
+  }
+
+  document.querySelectorAll('*[tabindex="-1"]').forEach((el) => {
+    el.setAttribute('aria-disabled', 'true');
+  });
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 import '../scss/app.scss';
-import { focusLoop } from './modules/_focusLoop.mjs';
+import { init } from './init.mjs';
 import './modules/Class/_FormValidation.mjs'
 import './modules/Class/_FocusLoop.mjs'
 import { GlobalMenu } from './modules/_globalMenu.mjs';
 import { headerStyleChanger } from './modules/_l-header-styleChange.mjs';
 import { swiper } from './modules/_swiper.js';
 import { btnPressToDisabled } from './modules/_btnPressToDisabled.mjs';
+
+init()
 
 headerStyleChanger();
 GlobalMenu();
