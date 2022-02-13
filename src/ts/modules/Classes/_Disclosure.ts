786c117c8aebe92ac0d5036151d8460d2e344288
上田貴央 <gibson511511@gmail.com>
2022-02-14 00:55:39 +0900

    test

--- src/ts/modules/Classes/_Disclosure.ts
+++ src/ts/modules/Classes/_Disclosure.ts
@@ -21,35 +21,54 @@ export default (() => {
 
 			summary.setAttribute('aria-expanded', 'false');
 			content.setAttribute('aria-hidden', 'true');
-			const contentHeight = content.clientHeight;
-			content.style.setProperty('height', '0');
 
 			summary.addEventListener('click', (e: MouseEvent) => {
 				e.preventDefault();
 				const self = e.currentTarget as HTMLElement;
 				const isOpen = self.getAttribute('aria-expanded') as 'true' | 'false' | null;
+
+				const reflow = (elm: HTMLElement) => {
+					elm.clientHeight;
+				};
+
 				switch (true) {
 					case (isOpen === null): {
 						return;
 					}
+					// 閉じる処理
 					case (isOpen === 'true'): {
 						self.setAttribute('aria-expanded', 'false');
 						self.classList.remove('is-open');
 						content.setAttribute('aria-hidden', 'true');
+						content.style.setProperty('height', `${content.scrollHeight}px`);
+						reflow(content);
 						content.style.setProperty('height', '0');
 						content.classList.remove('is-open');
+						content.classList.add('is-hidden');
 						break;
 					}
+					// 開く処理
 					case (isOpen === 'false'): {
 						self.setAttribute('aria-expanded', 'true');
 						self.classList.add('is-open');
 						content.setAttribute('aria-hidden', 'false');
-						content.style.setProperty('height', `${contentHeight}px`);
+
+						const height = content.scrollHeight;
+						content.style.setProperty('height', `${height}px`);
+						content.classList.remove('is-hidden');
+
 						content.classList.add('is-open');
 						break;
 					}
 				}
-			})
+			});
+
+			content.addEventListener('transitionend', (e: TransitionEvent) => {
+				const self = e.currentTarget as HTMLElement;
+				if (e.propertyName === 'height') {
+					self.style.removeProperty('height');
+				}
+			});
 
 		});
 	});
--- src/ts/modules/Classes/_Modal-module.ts
+++ src/ts/modules/Classes/_Modal-module.ts
@@ -3,155 +3,154 @@ const triggerSelector = 'data-modal-trigger';
 const closeBtnSelector = 'data-modal-close';
 
 class Modal {
-  private modal: HTMLElement;
-  private triggerElms!: NodeListOf<HTMLElement>;
-  private closeBtns!: NodeListOf<HTMLElement>
-  private transitionTime: number = 500;
-  private activeTrigger?: undefined | HTMLElement;
+	private modal: HTMLElement;
+	private triggerElms!: NodeListOf<HTMLElement>;
+	private closeBtns!: NodeListOf<HTMLElement>
+	private transitionTime: number = 500;
+	private activeTrigger?: undefined | HTMLElement;
 
-  constructor(modal: HTMLElement) {
-    this.modal = modal;
-  }
+	constructor(modal: HTMLElement) {
+		this.modal = modal;
+	}
 
-  setContentElms(triggerElms: NodeListOf<HTMLElement>, closeBtns: NodeListOf<HTMLElement>) {
-    this.triggerElms = triggerElms;
-    this.closeBtns = closeBtns;
-  }
+	setContentElms(triggerElms: NodeListOf<HTMLElement>, closeBtns: NodeListOf<HTMLElement>) {
+		this.triggerElms = triggerElms;
+		this.closeBtns = closeBtns;
+	}
 
-  setConfig() {
-    const transitionTime = this.modal.getAttribute('data-modal-transition');
-    const extractedNumber = transitionTime?.match(/[0-9]/g);
+	setConfig() {
+		const transitionTime = this.modal.getAttribute('data-modal-transition');
+		const extractedNumber = transitionTime?.match(/[0-9]/g);
 
-    if (
-      typeof extractedNumber !== 'undefined' &&
-      extractedNumber !== null
-      ) {
-      this.transitionTime = Number(extractedNumber.join(''));
-    }
+		if (
+			typeof extractedNumber !== 'undefined' &&
+			extractedNumber !== null
+		) {
+			this.transitionTime = Number(extractedNumber.join(''));
+		}
 
-    this.modal.style.setProperty('--transition-time', `${this.transitionTime}ms`);
-    const isOpen = this.modal.getAttribute('aria-hidden') === 'false' ? true : false;
-    this.triggerElms.forEach((trigger) => {
-      trigger.setAttribute('aria-controls', `${this.modal.id}`);
-      isOpen ? trigger.setAttribute('aria-expanded', 'true') : trigger.setAttribute('aria-expanded', 'false');
-    });
-  }
+		this.modal.style.setProperty('--transition-time', `${this.transitionTime}ms`);
+		const isOpen = this.modal.getAttribute('aria-hidden') === 'false' ? true : false;
+		this.triggerElms.forEach((trigger) => {
+			trigger.setAttribute('aria-controls', `${this.modal.id}`);
+			isOpen ? trigger.setAttribute('aria-expanded', 'true') : trigger.setAttribute('aria-expanded', 'false');
+		});
+	}
 
-  openModal(activeTrigger?: HTMLElement | HTMLElement) {
-    this.activeTrigger = activeTrigger;
-    if (typeof this.activeTrigger !== 'undefined') this.activeTrigger.setAttribute('aria-expanded', 'true');
-    this.modal.setAttribute('aria-hidden', 'false');
-    this.modal.focus();
-    document.body.classList.add('is-scrollLock');
+	openModal(activeTrigger?: HTMLElement | HTMLElement) {
+		this.activeTrigger = activeTrigger;
+		if (typeof this.activeTrigger !== 'undefined') this.activeTrigger.setAttribute('aria-expanded', 'true');
+		this.modal.setAttribute('aria-hidden', 'false');
+		this.modal.focus();
+		document.body.classList.add('is-scrollLock');
 
-    window.addEventListener('keydown', this.closeTypeEsc);
-  }
+		window.addEventListener('keydown', this.closeTypeEsc);
+	}
 
-  closeModal() {
-    this.modal.setAttribute('aria-hidden', 'true');
-    document.body.classList.remove('is-scrollLock');
+	closeModal() {
+		this.modal.setAttribute('aria-hidden', 'true');
+		document.body.classList.remove('is-scrollLock');
 
-    if (typeof this.activeTrigger !== 'undefined') {
-      this.activeTrigger.focus();
-      this.activeTrigger.setAttribute('aria-expanded', 'false');
-    }
+		if (typeof this.activeTrigger !== 'undefined') {
+			this.activeTrigger.focus();
+			this.activeTrigger.setAttribute('aria-expanded', 'false');
+		}
 
-    window.removeEventListener('keydown', this.closeTypeEsc);
-  }
+		window.removeEventListener('keydown', this.closeTypeEsc);
+	}
 
-  private closeTypeEsc = (e: KeyboardEvent) => {
-    if(
-      e.key === 'Escape' ||
-      e.key === 'Esc'
-    ) {
-      this.closeModal();
-    }
-  }
+	private closeTypeEsc = (e: KeyboardEvent) => {
+		if (
+			e.key === 'Escape' ||
+			e.key === 'Esc'
+		) {
+			this.closeModal();
+		}
+	}
 
-  addEvent() {
-    this.triggerElms.forEach((triggerElm) => {
-      triggerElm.addEventListener('click', (e: MouseEvent) => {
-        const clickedTrigger = e.currentTarget as HTMLElement;
-        this.openModal(clickedTrigger);
-      });
+	addEvent() {
+		this.triggerElms.forEach((triggerElm) => {
+			triggerElm.addEventListener('click', (e: MouseEvent) => {
+				this.openModal(<HTMLElement>e.currentTarget);
+			});
 
-      triggerElm.addEventListener('keydown', (e: KeyboardEvent) => {
-        if(
-          e.key === ' ' ||
-          e.key === 'Space'
-        ) {
-          e.preventDefault();
-          const activeTrigger = e.currentTarget as HTMLElement;
-          this.openModal(activeTrigger);
-        }
-      });
-    })
+			triggerElm.addEventListener('keydown', (e: KeyboardEvent) => {
+				if (
+					e.key === ' ' ||
+					e.key === 'Space'
+				) {
+					e.preventDefault();
+					const activeTrigger = e.currentTarget as HTMLElement;
+					this.openModal(activeTrigger);
+				}
+			});
+		})
 
-    this.closeBtns.forEach((closeBtn) => {
-      closeBtn.addEventListener('click', () => {
-        this.closeModal();
-      });
-      closeBtn.addEventListener('keydown', (e: KeyboardEvent) => {
-        if(
-          e.key === ' ' ||
-          e.key === 'Space'
-        ) {
-          e.preventDefault();
-          this.closeModal();
-        }
-      });
-    });
+		this.closeBtns.forEach((closeBtn) => {
+			closeBtn.addEventListener('click', () => {
+				this.closeModal();
+			});
+			closeBtn.addEventListener('keydown', (e: KeyboardEvent) => {
+				if (
+					e.key === ' ' ||
+					e.key === 'Space'
+				) {
+					e.preventDefault();
+					this.closeModal();
+				}
+			});
+		});
 
-    const overlay = this.modal.querySelector('[data-modal-overlay]');
-    if (overlay !== null) {
-      overlay.addEventListener('click', () => {
-        this.closeModal();
-      });
-    }
-  }
+		const overlay = this.modal.querySelector('[data-modal-overlay]');
+		if (overlay !== null) {
+			overlay.addEventListener('click', () => {
+				this.closeModal();
+			});
+		}
+	}
 }
 
 // init
 (() => {
-  window.addEventListener('DOMContentLoaded', () => {
-    const modalTargets = document.querySelectorAll<HTMLElement>(`[${targetSelector}]`);
-    if (modalTargets.length === 0) return;
+	window.addEventListener('DOMContentLoaded', () => {
+		const modalTargets = document.querySelectorAll<HTMLElement>(`[${targetSelector}]`);
+		if (modalTargets.length === 0) return;
 
-    modalTargets.forEach((modalTarget) => {
-      const modal = new Modal(modalTarget);
-      const triggerElms = document.querySelectorAll<HTMLElement>(`[${triggerSelector}="${modalTarget.id}"]`);
-      const closeBtns = modalTarget.querySelectorAll<HTMLElement>(`[${closeBtnSelector}="${modalTarget.id}"]`);
+		modalTargets.forEach((modalTarget) => {
+			const modal = new Modal(modalTarget);
+			const triggerElms = document.querySelectorAll<HTMLElement>(`[${triggerSelector}="${modalTarget.id}"]`);
+			const closeBtns = modalTarget.querySelectorAll<HTMLElement>(`[${closeBtnSelector}="${modalTarget.id}"]`);
 
-      modal.setContentElms(triggerElms, closeBtns);
-      modal.setConfig();
-      modal.addEvent();
-    });
-  });
+			modal.setContentElms(triggerElms, closeBtns);
+			modal.setConfig();
+			modal.addEvent();
+		});
+	});
 })();
 
 const modalManage = {
-  /**
-   * 特定のモーダルを開く処理
-   * @param {string} modalId 開きたいモーダルのid
-   * @returns {void}
-   */
-  openModal: (modalId: string, triggerElm?: HTMLElement) => {
-    const targetModal = document.getElementById(modalId);
-    if (targetModal === null) return;
+	/**
+	 * 特定のモーダルを開く処理
+	 * @param {string} modalId 開きたいモーダルのid
+	 * @returns {void}
+	 */
+	openModal: (modalId: string, triggerElm?: HTMLElement) => {
+		const targetModal = document.getElementById(modalId);
+		if (targetModal === null) return;
 
-    new Modal(targetModal).openModal(triggerElm);
-  },
-  /**
-   * 特定のモーダルを閉じる処理
-   * @param {string} modalId 閉じたいモーダルのid
-   * @returns {void}
-   */
-  closeModal: (modalId: string) => {
-    const targetModal = document.getElementById(modalId);
-    if (targetModal === null) return;
+		new Modal(targetModal).openModal(triggerElm);
+	},
+	/**
+	 * 特定のモーダルを閉じる処理
+	 * @param {string} modalId 閉じたいモーダルのid
+	 * @returns {void}
+	 */
+	closeModal: (modalId: string) => {
+		const targetModal = document.getElementById(modalId);
+		if (targetModal === null) return;
 
-    new Modal(targetModal).closeModal();
-  }
+		new Modal(targetModal).closeModal();
+	}
 };
 
 export { modalManage };
